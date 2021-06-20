0d9b00cfc9148af5eb106c284e30bb9e885398c0
Dilli Bhandari <bhandaridilli1@gmail.com>
2021-06-20 10:36:17 +0545

    thene extracted complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:catalogapp/pages/homepage.dart';
 import 'package:catalogapp/pages/loginpage.dart';
 import 'package:catalogapp/utils/routes.dart';
+import 'package:catalogapp/widget/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,10 +15,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      // theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       initialRoute: MyRoutes.homeRoute,
       routes: {
         // "/": (context) => LoginPage(),
